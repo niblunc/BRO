51e3673534daf840048a93c50e5711f4e9d4c64b
nikkibytes <nicholletteacosta@gmail.com>
2019-10-07 16:06:27 -0400

    added output text file generator for volume check in bids

--- BIDS/.ipynb_checkpoints/bids_quality_check-checkpoint.py
+++ BIDS/.ipynb_checkpoints/bids_quality_check-checkpoint.py
@@ -2,51 +2,105 @@
 
 import os, glob
 import pandas as pd
-
+import subprocess
 
 
 def dict_make(sessions, sub_dirs):
     qa_dict = {}
+    vol_dict = {}
     # loop through each sessions we have
+    #outfile=open("/projects/niblab/bids_projects/Experiments/bro/bids_/derivatives/bad_volumes.txt", 'w')
     for sess_id in sessions:
         if sess_id not in qa_dict:
             qa_dict[sess_id] = {}
+        if sess_id not in vol_dict:
+            vol_dict[sess_id] = {}
         # loop through subjects by their bids path
         for sub_path in sorted(sub_dirs):
             base_dir = os.path.join(sub_path, sess_id) 
             sub_id = sub_path.split("/")[-1]
             if sub_id not in qa_dict[sess_id]:
                 qa_dict[sess_id][sub_id] = { "func": None, "runs_pe": None, "runs_train": None, "runs_rest": None, "anat": None, "fmap": None }
-                #CHECK FOR DIRECTORIES
-            # func/
+            
+                
+            # Functional files: func/
             funcs_nii=glob.glob("/projects/niblab/bids_projects/Experiments/bro/bids_/{}/{}/func/*.nii.gz".format(sub_id, sess_id))
             #print(funcs_nii)
+            bad_funcs = []
+            
+            # add values to qa_dict -
             nii_ct = len(funcs_nii)
             qa_dict[sess_id][sub_id]["func"] = nii_ct
             qa_dict[sess_id][sub_id]["runs_pe"] = len([x for x in funcs_nii if "-pe_" in x])
             qa_dict[sess_id][sub_id]["runs_train"] = len([x for x in funcs_nii if "training" in x])
             qa_dict[sess_id][sub_id]["runs_rest"] = len([x for x in funcs_nii if "resting" in x])
-            # anat/
+            
+            # volume check - 
+            for func in funcs_nii:
+                
+                filename=func.split("/")[-1]
+                task=func.split("/")[-1].split("_")[2]
+                if task != "resting":
+                    run_id = func.split('/')[-1].split('_')[3]
+                else:
+                    run_id = None
+                fsl_cmd ="fslnvols {}".format(func)
+                vol=subprocess.check_output(fsl_cmd, shell=True)
+                vol=str(vol,'utf-8').strip()
+                
+                if "-pe" in task:
+                    expected_vol = 193
+                elif "training" in task:
+                    expected_vol = 243
+                else:
+                    expected_vol = 147
+                if int(vol) != expected_vol:
+                    #print("File:{} \t\t Volume:{}".format(filename, vol))
+                    temp_tuple=(filename, vol)
+                    #print("Found bad functional file...........")
+                    if "training" in task:
+                        line="File:{} \t Volume:{}".format(filename, vol)
+                        print(line)
+                    else:
+                        line="File:{} \t\t Volume:{}".format(filename, vol)
+                        print(line)
+                    #outfile.write(line+"\n")
+
+                    bad_funcs.append(temp_tuple)
+            
+            
+            # if we found bad files, do something with them here
+            if bad_funcs:
+                #print("ID:{} \t {}".format(sub_id,bad_funcs))
+                if sub_id not in vol_dict[sess_id]:  
+                    vol_dict[sess_id][sub_id] = {}
+                vol_dict[sess_id][sub_id]["FILES"] = bad_funcs
+               
+            # Anatomical files: anat/
             anat_nii=glob.glob("/projects/niblab/bids_projects/Experiments/bro/bids_/{}/{}/anat/*.nii.gz".format(sub_id, sess_id))
             #print(anat_nii)
+            # add value to dict-
             nii_ct = len(anat_nii)
             qa_dict[sess_id][sub_id]["anat"] = nii_ct
-            # fmap/
+            
+            # Field mapping files: fmap/
             fmap_nii=glob.glob("/projects/niblab/bids_projects/Experiments/bro/bids_/{}/{}/func/*.nii.gz".format(sub_id, sess_id))
             #print(fmap_nii)
+            # add value to dict-
             nii_ct = len(fmap_nii)
             qa_dict[sess_id][sub_id]["fmap"] = nii_ct
         
-        print("SESSION {} DICTIONARY: \n{}\n".format(sess_id,qa_dict[sess_id]))
- 
+        #print("SESSION {} DICTIONARY: \n{}\n".format(sess_id,qa_dict[sess_id]))
+    #print(vol_dict) 
+    #outfile.close()
     return qa_dict
         
         
 def analyze_data(qa_dict):
     for sess_id in qa_dict:
         df = pd.DataFrame(qa_dict[sess_id])
-        print(df.head())
-        df.T.to_csv("bro_bids_{}.csv".format(sess_id), sep="\t")
+        #print(df.head())
+        #df.T.to_csv("bro_bids_{}.csv".format(sess_id), sep="\t")
     
 def main():
     # get paths and subject directory paths
@@ -56,5 +110,5 @@ def main():
     # get the multiple sessions
     sessions=['ses-1', 'ses-2']
     qa_dict = dict_make(sessions, sub_dirs) 
-    analyze_data(qa_dict)
+    #analyze_data(qa_dict)
 main()
---
+++ untitled.md
@@ -0,0 +1,9 @@
+# Bro data specs
+
+## General:  
+Expected Volumes:  
+* Func/  
+  * task-pe_: 193  
+  * task-training: 243  
+  * task-resting: 147
+  
